5074bf4cde3b7cf6a44e71fbd35c281def7d7d29
Juan Jose Osorio Arango <jjosorioa3@eafit.edu.co>
2026-02-06 23:42:57 -0500

    cambios en la funcion home

--- movie/views.py
+++ movie/views.py
@@ -5,7 +5,7 @@ from django.http import HttpResponse
 def home(request):
     #return HttpResponse("<h1>Welcome to home page</h1>")
     #return render(request, 'home.html')
-     return render(request, 'home.html',{'name':'Juan Osorio'})
+     return render(request, 'home.html',{'name':'Juan Jose Osorio'})
 
 def about(request): 
     return HttpResponse("<h1>Welcome to about page</h1>")
