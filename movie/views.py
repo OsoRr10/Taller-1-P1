ec49c93da2d136f3d120d20aa7dc210121c00cdd
Juan Jose Osorio Arango <jjosorioa3@eafit.edu.co>
2026-02-06 23:51:58 -0500

    Modificacion de funcion about

--- movie/views.py
+++ movie/views.py
@@ -8,4 +8,4 @@ def home(request):
      return render(request, 'home.html',{'name':'Juan Jose Osorio'})
 
 def about(request): 
-    return HttpResponse("<h1>Welcome to about page</h1>")
+    return render(request, 'about.html')
